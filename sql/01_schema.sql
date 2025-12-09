75b76b5f4ccbb2bdf3562042b449a86a5fc156da
preethim10 <preethicse.m@gmail.com>
2025-12-09 03:30:51 -0800

    Create 01_schema.sql

---
+++ sql/01_schema.sql
@@ -0,0 +1,10 @@
+-- Ticket table schema
+CREATE TABLE tickets (
+    ticket_id INT,
+    created_at TIMESTAMP,
+    resolved_at TIMESTAMP,
+    priority VARCHAR(10),
+    agent_id VARCHAR(20),
+    channel VARCHAR(20),
+    status VARCHAR(20)
+);
